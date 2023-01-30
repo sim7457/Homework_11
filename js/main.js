90a38ba575ca2bf5de1d353e8339f1b347debe37
sim7457 <118698648+sim7457@users.noreply.github.com>
2023-01-30 17:47:11 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+    AOS.init();
+
+
+
+
+    $('.main_slide').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        dots: true,
+    });
+});
